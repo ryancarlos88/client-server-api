63e9d160e41793f73fb6a5b07208af1e97d25ac0
Ryan Dias <ryancarlos88@gmail.com>
2023-08-07 17:01:13 -0300

    adding db logic

--- server/server.go
+++ server/server.go
@@ -4,21 +4,44 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func main(){
-	http.HandleFunc("/cotacao", cotacaoHandler)
+type Server struct {
+	db *gorm.DB
+}
+
+func NewServer() *Server {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		checkErr(err)
+	}
+	return &Server{db}
+}
+
+func main() {
+	log.Println("iniciando server")
+	s := NewServer()
+
+	s.db.Table("cotacoes").AutoMigrate(&Cotacao{})
+
+	http.HandleFunc("/cotacao", s.cotacaoHandler)
 	http.ListenAndServe(":8080", nil)
 }
+
 const (
 	apiCallTimeout = time.Millisecond * 200
 	dbWriteTimeout = time.Millisecond * 10
-	apiUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	apiUrl         = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 )
-func cotacaoHandler(w http.ResponseWriter, r *http.Request){
-	
+
+func (s *Server) cotacaoHandler(w http.ResponseWriter, r *http.Request) {
+
 	apiCtx, cancel := context.WithTimeout(context.Background(), apiCallTimeout)
 	defer cancel()
 
@@ -31,36 +54,38 @@ func cotacaoHandler(w http.ResponseWriter, r *http.Request){
 	bodyBytes, err := io.ReadAll(res.Body)
 	checkErr(err)
 
-	var c Cotacao
+	var a ApiResponse
 
-	err = json.Unmarshal(bodyBytes, &c)
+	err = json.Unmarshal(bodyBytes, &a)
 	checkErr(err)
 
+	dbCtx, cancel := context.WithTimeout(context.Background(), dbWriteTimeout)
+	defer cancel()
 
-
-	w.Write([]byte(c.Usdbrl.Bid))
-
+	s.db.WithContext(dbCtx).Table("cotacoes").Create(&a.Cotacao)
 	
+	w.Write([]byte(a.Cotacao.Bid))
 }
-
 
 type Cotacao struct {
-	Usdbrl struct {
-		Code       string `json:"code"`
-		Codein     string `json:"codein"`
-		Name       string `json:"name"`
-		High       string `json:"high"`
-		Low        string `json:"low"`
-		VarBid     string `json:"varBid"`
-		PctChange  string `json:"pctChange"`
-		Bid        string `json:"bid"`
-		Ask        string `json:"ask"`
-		Timestamp  string `json:"timestamp"`
-		CreateDate string `json:"create_date"`
-	} `json:"USDBRL"`
+	ID         int    `gorm:"primaryKey"`
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+type ApiResponse struct {
+	Cotacao Cotacao `json:"USDBRL"`
 }
 
-func checkErr(e error){
+func checkErr(e error) {
 	if e != nil {
 		panic(e)
 	}
